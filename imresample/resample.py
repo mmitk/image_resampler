b408f2e0857493ca442ffc66579e0830edb97cb9
mmitk <mmitkov@comcast.net>
2020-11-16 13:35:29 -0500

    initial test suite complete and passed

--- imresample/resample.py
+++ imresample/resample.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
 import os
+import pathlib
 import sys
 import shutil
 import time
-from exceptions shapeException, inputException
+from .exceptions import shapeException, inputException
 import imblearn as imb
 
-resample_strategies = imb
+oversampling_strategies = imb.over_sampling
+undersampling_strategies = imb.under_sampling
 
 def resample_to_directory(resampler, src, target_directory, target_size = (64,64)):
     """Perform sampling on an image dataset using imbalanced-learn's resampler
@@ -35,7 +37,7 @@ def resample_to_directory(resampler, src, target_directory, target_size = (64,64
 
     images_arr, targets, class_encoding = load_directory(src, target_size)
 
-    images_arr.reshape(images_arr.shape[0],images_arr.shape[1]*images_arr.shape[2]*images_arr.shape[3])
+    images_arr = images_arr.reshape(images_arr.shape[0],images_arr.shape[1]*images_arr.shape[2]*images_arr.shape[3])
     targets = targets.reshape(-1,1)
 
     X, y = resampler.fit_resample(images_arr, targets)
@@ -51,7 +53,7 @@ def resample_image_set(resampler, images_array, targets_array, x_reshape = False
     """
     """
     if x_reshape:
-        images_array = images_array.reshape(images_arrayshape[0], images_array[1]*images_array.shape[2]*images_array.shape[3])
+        images_array = images_array.reshape(images_array.shape[0], images_array.shape[1]*images_array.shape[2]*images_array.shape[3])
     if y_reshape:
         targets_array = targets_array.reshape(-1,1)
 
@@ -67,17 +69,17 @@ def load_directory(directory_path, target_size = (64,64)):
     images_list = list()
     targets_list = list()
 
-    for count, folder in enumerate(os.istdir(directory_path)):
+    for count, folder in enumerate(os.listdir(directory_path)):
         if os.path.isdir(os.path.join(directory_path, folder)):
             class_encoding[count] = folder
     
     for key in class_encoding:
         class_directory = os.path.join(directory_path, class_encoding[key])
-        for image in os.listdir(class_directory)
-            print('.')
+        for image in os.listdir(class_directory):
+            print('.', end='')
             image = os.path.join(class_directory, image)
             loaded_image = cv2.imread(image)
-            loaded_image = loaded_image.resize(loaded_image, target_size, interpolation=cv2.INTER_CUBIC)
+            loaded_image = cv2.resize(loaded_image, target_size, interpolation=cv2.INTER_CUBIC)
             images_list.append(loaded_image)
             targets_list.append(key)
     
@@ -87,12 +89,13 @@ def load_directory(directory_path, target_size = (64,64)):
 
 
 
-def write_to_directory(image_array, target_array, target_directory, class_encoding = None, x_reshape = False, y_reshape = False, target_size = None):
+def write_to_directory(image_array, target_array, target_directory, class_encoding = None, x_reshape = False, y_reshape = False, target_size = None, write_format='jpg'):
     if not os.path.isdir(target_directory):
-        os.mkdir(target_directory)
+        pathlib.Path(target_directory).mkdir(parents = True, exist_ok=True)
 
-    if reshape:
+    if x_reshape:
         image_arry = image_array.reshape(image_array.shape[0], target_size[0], target_size[1], 3)
+    if y_reshape:
         target_array = target_array.reshape(-1,1)
     
     for count, img in enumerate(image_array):
@@ -104,11 +107,11 @@ def write_to_directory(image_array, target_array, target_directory, class_encodi
         if not os.path.isdir(class_directory):
             os.mkdir(class_directory)
         
-        image_path = os.path.join(class_directory, 'img{}'.format(count))
+        image_path = os.path.join(class_directory, 'img{}.{}'.format(count,write_format))
 
         try:
             cv2.imwrite(image_path, img)
-        except Exception:
+        except Exception as e:
             continue
 
 
---
+++ tests/test_suite.py
@@ -0,0 +1,73 @@
+import test_binary
+import test_categorical
+import sys
+import pathlib
+
+import imresample.resample
+
+def test_binary_all():
+    ros = imresample.resample.oversampling_strategies.RandomOverSampler()
+    test_binary.test_resample_binary(ros, 'ROS_BINARY')
+
+    adsn = imresample.resample.oversampling_strategies.ADASYN()
+    test_binary.test_resample_binary(adsn, 'ADASYN_BINARY')
+
+    bdln = imresample.resample.oversampling_strategies.BorderlineSMOTE()
+    test_binary.test_resample_binary(bdln, 'bdlnSMOTE_BINARY')
+
+    kms = imresample.resample.oversampling_strategies.KMeansSMOTE()
+    test_binary.test_resample_binary(kms, 'KMEANSSMOTE_BINARY')
+
+    sm = imresample.resample.oversampling_strategies.SMOTE()
+    test_binary.test_resample_binary(sm, 'SMOTE_BINARY')
+
+    cc = imresample.resample.undersampling_strategies.ClusterCentroids()
+    test_binary.test_resample_binary(cc, 'ClustCent_BINARY')
+
+    enn = imresample.resample.undersampling_strategies.EditedNearestNeighbours()
+    test_binary.test_resample_binary(enn, 'ENN_BINARY')
+
+    allknn = imresample.resample.undersampling_strategies.AllKNN()
+    test_binary.test_resample_binary(allknn, 'ALLKNN_BINARY')
+
+    iht = imresample.resample.undersampling_strategies.InstanceHardnessThreshold()
+    test_binary.test_resample_binary(iht, 'IntsHardThresh_BINARY')
+
+    nm = imresample.resample.undersampling_strategies.NearMiss()
+    test_binary.test_resample_binary(nm, 'NearMiss_BINARY')
+
+def test_categorical_all():
+    ros = imresample.resample.oversampling_strategies.RandomOverSampler()
+    test_categorical.test_resample_categorical(ros, 'ROS_categorical')
+
+    adsn = imresample.resample.oversampling_strategies.ADASYN()
+    test_categorical.test_resample_categorical(adsn, 'ADASYN_categorical')
+
+    bdln = imresample.resample.oversampling_strategies.BorderlineSMOTE()
+    test_categorical.test_resample_categorical(bdln, 'bdlnSMOTE_categorical')
+
+    kms = imresample.resample.oversampling_strategies.KMeansSMOTE()
+    test_categorical.test_resample_categorical(kms, 'KMEANSSMOTE_categorical')
+
+    sm = imresample.resample.oversampling_strategies.SMOTE()
+    test_categorical.test_resample_categorical(sm, 'SMOTE_categorical')
+
+    cc = imresample.resample.undersampling_strategies.ClusterCentroids()
+    test_categorical.test_resample_categorical(cc, 'ClustCent_categorical')
+
+    enn = imresample.resample.undersampling_strategies.EditedNearestNeighbours()
+    test_categorical.test_resample_categorical(enn, 'ENN_categorical')
+
+    allknn = imresample.resample.undersampling_strategies.AllKNN()
+    test_categorical.test_resample_categorical(allknn, 'ALLKNN_categorical')
+
+    iht = imresample.resample.undersampling_strategies.InstanceHardnessThreshold()
+    test_categorical.test_resample_categorical(iht, 'IntsHardThresh_categorical')
+
+    nm = imresample.resample.undersampling_strategies.NearMiss()
+    test_categorical.test_resample_categorical(nm, 'NearMiss_categorical')
+
+
+if __name__ == '__main__':
+    test_binary_all()
+    test_categorical_all()
